a49190a0fdf509ae7b60034e255b85c7c451da36
Dzhabee <gogolsketchup@gmail.com>
2018-11-21 21:27:32 +0400

    Поправил второе задание с учетом ТЗ и замечаний

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,3 @@
-/* Все поправил кроме первого замечания.
-Как ты и сказал, отнял от облака высоту столбца CLOUD_HEIGHT - BAR_HEIGHT - TEXT_WIDTH (стр 56)
-Но все равно отрисовывает сверху - вниз((
-*/
 'use strict';
 
 var CLOUD_WIDTH = 420;
@@ -10,9 +6,10 @@ var CLOUD_RENDER = 100;
 var CLOUD_X = 140;
 var GAP_CLOUD = 10;
 var GAP = 50;
-var TEXT_WIDTH = 20;
-var BAR_WIDTH = 40;
-var BAR_HEIGHT = 150;
+var TEXT_GAP = 20;
+var HISTOGRAM_WIDTH = 40;
+var HISTOGRAM_HEIGHT = 150;
+var TEXT_COLOR = '#000';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -37,24 +34,23 @@ var getRndInteger = function (min, max) {
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_RENDER + GAP_CLOUD, GAP_CLOUD * 2, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_RENDER, GAP_CLOUD, '#fff');
-
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = TEXT_COLOR;
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X - TEXT_WIDTH, BAR_WIDTH);
-  ctx.fillText('Список результатов:', CLOUD_X - TEXT_WIDTH, BAR_WIDTH + TEXT_WIDTH);
+  ctx.fillText('Ура вы победили!', CLOUD_X - TEXT_GAP, HISTOGRAM_WIDTH);
+  ctx.fillText('Список результатов:', CLOUD_X - TEXT_GAP, HISTOGRAM_WIDTH + TEXT_GAP);
 
   var maxTime = getMaxElement(times);
 
-  ctx.fillStyle = '#000';
   for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(players[i], CLOUD_X + (HISTOGRAM_WIDTH + GAP) * i, CLOUD_HEIGHT);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + (HISTOGRAM_WIDTH + GAP) * i, CLOUD_HEIGHT - (HISTOGRAM_HEIGHT * times[i] / maxTime) - GAP + TEXT_GAP);
     if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = 'rgba(255, 0, 0)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, ' + getRndInteger(1, 255) + ', 1)';
+      ctx.fillStyle = 'rgba(0, 0, ' + getRndInteger(1, 255) + ')';
     }
-    ctx.fillText(players[i], CLOUD_X + (BAR_WIDTH + GAP) * i, CLOUD_HEIGHT);
-    ctx.fillRect(CLOUD_X + (BAR_WIDTH + GAP) * i, CLOUD_HEIGHT - BAR_HEIGHT - TEXT_WIDTH, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + (BAR_WIDTH + GAP) * i, BAR_WIDTH + GAP);
+    ctx.fillRect(CLOUD_X + (HISTOGRAM_WIDTH + GAP) * i, CLOUD_HEIGHT - (HISTOGRAM_HEIGHT * times[i] / maxTime) - TEXT_GAP, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT * times[i] / maxTime);
   }
 };
 
